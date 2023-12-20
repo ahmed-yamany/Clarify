8a4be33d584d70695e53197cce71686937909dc1
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2023-12-20 21:34:42 +0200

    Initial Commit

---
+++ Clarity/ClarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClarityApp.swift
+//  Clarity
+//
+//  Created by Ahmed Yamany on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
